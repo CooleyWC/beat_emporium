803cc094b130b71f0420219941d9f73205ce1379
Will Cooley <william.cooley@gmail.com>
2024-05-10 22:48:01 -0400

    added faker and flask-login

--- server/seed.py
+++ server/seed.py
@@ -1,6 +1,8 @@
 from config import app, db
+from faker import Faker
 
 if __name__ == "__main__":
   with app.app_context():
     pass
+    # fake = Faker()
     # remove pass and write your seed data
