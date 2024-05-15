7118f50c907e14cc5dac3a6eec7d43406d0b857c
Will Cooley <william.cooley@gmail.coom>
2024-05-14 22:17:35 -0400

    adjusted serialize_rules and reseeded the database

--- server/models/rentals.py
+++ server/models/rentals.py
@@ -7,9 +7,8 @@ from config import db
 class Rental(db.Model, SerializerMixin):
     __tablename__ = 'rentals'
 
-    serialize_rules = ('-user.rentals', '-user.reviews', '-user.instruments', '-instrument.rentals', '-instrument.reviews', '-review',)
-
-    # serialize_rules = ('-user.rentals','-instrument', '-review',)
+    serialize_rules = ('-user.rentals', '-user.reviews', '-user.instruments', 
+                       '-instrument.rentals', '-instrument.reviews', '-review',)
 
     id=db.Column(db.Integer, primary_key=True)
 
@@ -23,10 +22,6 @@ class Rental(db.Model, SerializerMixin):
     user = db.relationship('User', back_populates='rentals')
     instrument = db.relationship('Instrument', back_populates='rentals')
     review = db.relationship('Review', back_populates='rentals', cascade='all, delete-orphan')
-
-    # user = db.relationship('User', back_populates='rentals')
-    # instrument = db.relationship('Instrument', back_populates='rentals')
-    # review = db.relationship('Review', back_populates='rentals', cascade='all, delete-orphan')
 
     def __repr__(self):
         user=self.user.username if self.user else None
--- server/seed.py
+++ server/seed.py
@@ -29,11 +29,13 @@ if __name__ == "__main__":
     db.session.commit()
 
     print('adding instruments...')
-    instrument_1 = Instrument(name='Snare Drum', brand='Pearl', model='Philharmonic', size='14', color='Maple', description=fake.paragraph(nb_sentences=5), image='https://images.unsplash.com/photo-1626962131658-603aae425e19?q=80&w=3560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', for_rent=True, rent_price=10.50, sale_price=400, in_stock=True)
+    instrument_1 = Instrument(name='Snare Drum', brand='Pearl', model='Philharmonic', size='14x5', color='Maple', description=fake.paragraph(nb_sentences=5), image='https://images.unsplash.com/photo-1626962131658-603aae425e19?q=80&w=3560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', for_rent=True, rent_price=10.50, sale_price=400, in_stock=True)
     instrument_2 = Instrument(name='Marimba', brand='Marimba One', model='Standard', size='5 Octave', color='Natural Wood', description=fake.paragraph(nb_sentences=5), image='https://images.unsplash.com/photo-1635737775897-cbc4c19a697d?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bWFyaW1iYXxlbnwwfHwwfHx8MA%3D%3D', for_rent=True, rent_price=70, sale_price=12000, in_stock=True)
-    instrument_3 = Instrument(name='Tambourine', brand='Black Swamp', model='German Silver', size='10"', color='Natural Wood', description=fake.paragraph(nb_sentences=5), image='https://plus.unsplash.com/premium_photo-1702552106545-feef7cbecfe0?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', for_rent=True, rent_price=10.50, sale_price=400, in_stock=True)
+    instrument_3 = Instrument(name='Tambourine', brand='Black Swamp', model='German Silver', size='10in', color='Natural Wood', description=fake.paragraph(nb_sentences=5), image='https://plus.unsplash.com/premium_photo-1702552106545-feef7cbecfe0?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', for_rent=True, rent_price=10.50, sale_price=400, in_stock=True)
+    instrument_4 = Instrument(name='Triangle', brand='Alan Abel', model='Alan Abel', size='9in', color='silver', description=fake.paragraph(nb_sentences=5), image='https://images.unsplash.com/photo-1511447333015-45b65e60f6d5?q=80&w=3655&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', for_rent=True, rent_price=4.00, sale_price=50.75, in_stock=True)
 
-    db.session.add_all([instrument_1, instrument_2, instrument_3])
+
+    db.session.add_all([instrument_1, instrument_2, instrument_3, instrument_4])
     db.session.commit()
 
     print('adding rentals...')
@@ -41,15 +43,18 @@ if __name__ == "__main__":
     rental_1 = Rental(user_id=1, instrument_id=1, created_at=datetime(2024,3,1,10,10,10), start_date=date(2024, 4, 14), return_date=date(2024, 5, 10))
     rental_2 = Rental(user_id=2, instrument_id=2, created_at=datetime(2024,5,10,11,1,0), start_date=date(2024, 5, 14), return_date=date(2024, 6, 1))
     rental_3 = Rental(user_id=3, instrument_id=3, created_at=datetime(2024,6,3,11,5,0), start_date=date(2024, 6, 27), return_date=date(2024, 7, 1))
+    rental_4 = Rental(user_id=2, instrument_id=4, created_at=datetime(2024,5,11,10,1,0), start_date=date(2024, 6, 14), return_date=date(2024, 7, 1))
 
-    db.session.add_all([rental_1, rental_2, rental_3])
+    db.session.add_all([rental_1, rental_2, rental_3, rental_4])
     db.session.commit()
 
     print('adding reviews')
 
-    review_1 = Review(user_id=1, instrument_id=1, rental_id=1, created_at=datetime(2024,8,10,10,10,0))
-    review_2 = Review(user_id=2, instrument_id=2, rental_id=2, created_at=datetime(2024,8,10,10,10,0))
+    review_1 = Review(user_id=1, instrument_id=1, rental_id=1, created_at=datetime(2024,8,10,10,10,0), content=fake.paragraph(nb_sentences=5))
+    review_2 = Review(user_id=2, instrument_id=2, rental_id=2, created_at=datetime(2024,8,10,10,10,0), content=fake.paragraph(nb_sentences=5))
+    review_3 = Review(user_id=2, instrument_id=4, rental_id=4, created_at=datetime(2024,9,10,10,10,0), content=fake.paragraph(nb_sentences=5))
 
-    db.session.add_all([review_1, review_2])
+
+    db.session.add_all([review_1, review_2, review_3])
     db.session.commit()
 
