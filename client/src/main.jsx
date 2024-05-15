13ffd8ed6acd1cc44e079720eab4e67159a1b915
Will Cooley <william.cooley@gmail.com>
2024-05-15 14:51:10 -0400

    added basic starting components, AuthProvider, and set up client side routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import routes from './routes';
+import {AuthProvider} from './components/context/AuthProvider'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const router = createBrowserRouter(routes)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <AuthProvider>
+    <RouterProvider router={router} />
+  </AuthProvider>
 )
