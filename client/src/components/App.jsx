5f6c5042b4a09c4cc6d5d934a26736e22145e823
Will Cooley <william.cooley@gmail.com>
2024-05-17 12:57:20 -0400

    added instrument cards to instruments

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -42,7 +42,7 @@ function App() {
   return (
     <>
       <Header />
-      <Outlet />
+      <Outlet context={{allInstruments}}/>
     </>
   )
 }
--- client/src/components/cards/InstrumentCard.jsx
+++ client/src/components/cards/InstrumentCard.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
+import {Card, CardContent, Typography, CardMedia} from '@mui/material' 
 
-function InstrumentCard() {
+function InstrumentCard({name, description, for_rent, image, model, rent_price, reviews, sale_price, size}) {
     return (
-        <div>
-            
-        </div>
+        <Card sx={{maxWidth: '400px', minHeight: '600px', maxHeight: '600px'}}>
+            <CardMedia 
+                component='img'
+                image={image}
+                sx={{height: '200px'}}
+            />
+
+            <CardContent>
+                <Typography>
+                    {name}
+                </Typography>
+                <Typography>
+                    {model}
+                </Typography>
+                <Typography>
+                    {size}
+                </Typography>
+                <Typography>
+                    {description}
+                </Typography>
+                <Typography>
+                    {rent_price}
+                </Typography>
+            </CardContent>
+        </Card>
     );
 }
 
 export default InstrumentCard;
+
+
+// key={instrument.id}
+//                         name={instrument.name}
+//                         description={instrument.description}
+//                         for_rent={instrument.for_rent}
+//                         image={instrument.image}
+//                         model={instrument.modal}
+//                         rent_price={instrument.rent_price}
+//                         reviews={instrument.reviews}
+//                         sale_price={instrument.sale_price}
+//                         size={instrument.size}
