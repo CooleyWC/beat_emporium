503e8246990266df0745c55361f7482254cec03a
Will Cooley <william.cooley@gmail.com>
2024-05-17 13:16:26 -0400

    added user profile card to dashboard

--- client/src/components/pages/Dashboard.jsx
+++ client/src/components/pages/Dashboard.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import {Box, Typography} from '@mui/material'
+import UserProfileCard from '../cards/UserProfileCard';
+import { useAuth } from '../context/AuthProvider';
+
 
 function Dashboard() {
+
+    const {user} = useAuth()
+
+    if(user===null || !user){
+        return <p>loading...</p>
+    }
+
     return (
+        <>
         <Box sx={{marginTop: '100px'}}>
             <Typography>Dashboard</Typography>
         </Box>
+        <Box>
+            <UserProfileCard 
+                key={user.id}
+                first_name={user.first_name}
+                last_name={user.last_name}
+                email={user.email}
+                location={user.location}
+            />
+        </Box>
+        </>
     );
 }
 
