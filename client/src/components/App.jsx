a4c4e3b25f71813b3c64088060196fead1f74007
Will Cooley <william.cooley@gmail.coom>
2024-05-17 11:38:30 -0400

    added check session to App

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {Outlet} from 'react-router-dom'
 import Header from './Header'
+import { useAuth } from './context/AuthProvider';
+
 
 
 
 function App() {
 
+  const {user, login, logout, update} = useAuth();
+
+  useEffect(()=>{
+    checkUser()
+  }, [])
+
+  const checkUser = async () =>{
+    try{ 
+      const res = await fetch('/api/check_session')
+      const userData = await res.json()
+      if(res.ok){
+        login(userData)
+      } else {
+        logout()
+      }
+    } catch (error) {
+      console.error('Error - try logging in again', error.message)
+    }
+  }
 
   return (
     <>
