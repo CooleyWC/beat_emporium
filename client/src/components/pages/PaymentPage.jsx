64a88e622cebd7835520cd9c33d72771f7d6fd1e
Will Cooley <william.cooley@gmail.coom>
2024-05-24 14:56:55 -0400

    linked instrument items from instrument page through payment page

--- client/src/components/pages/PaymentPage.jsx
+++ client/src/components/pages/PaymentPage.jsx
@@ -1,14 +1,18 @@
-import React, {useState, useCallback} from 'react';
+import React, {useCallback} from 'react';
 import {loadStripe} from '@stripe/stripe-js';
 import {
     EmbeddedCheckoutProvider,
     EmbeddedCheckout
 } from '@stripe/react-stripe-js'
 import {Box} from '@mui/material'
+import { useOutletContext } from 'react-router-dom';
 
 const stripePromise = loadStripe('pk_test_51PIh6FRooiRlSIbzyMN3RxjtLClLzzjm4d0L2Ne7QjysYBIYx1lNYpVe2lWFgSL3UF2aj97ZkMufZRVTpzhWeZiF009APCTydE');
 
 const PaymentPage = () =>{
+
+  const {cartItems} = useOutletContext()
+  console.log(`from payment page: ${cartItems}`)
 
   const fetchClientSecret = useCallback(() => {
      
@@ -44,31 +48,3 @@ const PaymentPage = () =>{
 }
 
 export default PaymentPage
-
-// const PaymentPage = () =>{
-
-//     const fetchClientSecret = useCallback(() => {
-       
-//         return fetch("/create_checkout_session", {
-//           method: "POST",
-//         })
-//           .then((res) => res.json())
-//           .then((data) => data.clientSecret);
-//       }, []);
-    
-//       const options = {fetchClientSecret};
-    
-//       return (
-//         <Box sx={{marginTop: '100px'}} id="checkout">
-//           <EmbeddedCheckoutProvider
-//             stripe={stripePromise}
-//             options={options}
-//           >
-//             <EmbeddedCheckout />
-//           </EmbeddedCheckoutProvider>
-//         </Box>
-//       )
-
-// }
-
-// export default PaymentPage
