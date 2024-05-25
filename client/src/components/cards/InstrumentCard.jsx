c025030cf2e639593632ffbc90b827eb7379782a
Will Cooley <william.cooley@gmail.com>
2024-05-25 15:32:05 -0400

    added dialog box to InstrumentCard

--- client/src/components/cards/InstrumentCard.jsx
+++ client/src/components/cards/InstrumentCard.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Card, CardContent, Typography, CardMedia, Button} from '@mui/material'
 import { useAuth } from '../context/AuthProvider';
 import { useOutletContext } from 'react-router-dom';
+// new below
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 
 function InstrumentCard({name, description, for_rent, image, model, 
     rent_price, reviews, sale_price, size, instrumentObj, currentRentals}) {
+
+    const [open, setOpen] = useState(false);
+    const [dateInput, setDateInput] = useState('')
+    const [selection, setSelection] = useState('')
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+      
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     const {user} = useAuth()
     const{handleCartItems, cartItems, handleRemoveCartItems} = useOutletContext();
@@ -19,6 +38,8 @@ function InstrumentCard({name, description, for_rent, image, model,
             alert('You must be logged in to add this to your cart')
             return
         }
+
+        handleClickOpen()
         
         handleCartItems(instrumentObj)
     }
@@ -33,10 +54,10 @@ function InstrumentCard({name, description, for_rent, image, model,
         )
     })
 
-    console.log(name)
-    console.log(rentalDates)
+    console.log(selection)
 
     return (
+        <>
         <Card sx={{maxWidth: '400px', minHeight: '600px', maxHeight: '600px'}}>
             <CardMedia 
                 component='img'
@@ -62,6 +83,37 @@ function InstrumentCard({name, description, for_rent, image, model,
             </CardContent>
             {checkIfItemInCart ? <Button onClick={handleRemove}>Remove From Cart</Button>: <Button onClick={handleAdd}>Add To Cart</Button>}
         </Card>
+        <Dialog
+            open={open}
+            onClose={handleClose}
+            PaperProps={{
+                component: 'form',
+                onSubmit: (e)=>{
+                    e.preventDefault()
+                    setSelection(dateInput)
+                    handleClose()
+                }
+            }}
+        >
+            <DialogTitle>Select Dates</DialogTitle>
+            <DialogContent>
+            <DialogContentText>Select your rental duration for: {name}</DialogContentText>
+            <TextField
+                id='date-select'
+                label='Select Date'
+                fullWidth
+                variant='standard'
+                value={dateInput}
+                onChange={(e)=>setDateInput(e.target.value)}
+            />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button type='submit'>Complete Add To Cart</Button>
+            </DialogActions>
+
+        </Dialog>
+        </>
     );
 }
 
