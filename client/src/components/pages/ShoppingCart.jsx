b4500277772fb83be9475e136f116b3731dfd16d
Will Cooley <william.cooley@gmail.com>
2024-05-29 15:34:04 -0400

    change to shopping cart

--- client/src/components/pages/ShoppingCart.jsx
+++ client/src/components/pages/ShoppingCart.jsx
@@ -12,9 +12,7 @@ function ShoppingCart() {
     // console.log('cart items from shopping cart', cartItems)
 
     const itemsToDisplay = cartItems.map((item)=>{
-        
-        return (
-            
+        return ( 
             <CartCard 
                 key={item.id}
                 itemObj={item}
@@ -26,28 +24,19 @@ function ShoppingCart() {
     })
 
     const handleCheckoutClick = ()=>{
-        if(cartItems.length == 0){
-            console.log('you dont have any items in your cart')
-            return
-        }
         navigate('/payment_page')
     }
 
     return (
      
         <Box style={{marginTop: '100px'}}>
-          
             <Typography>Shopping Cart</Typography>
-        
-            {cartItems.length == 0 ? <Typography>No items in cart</Typography> :(
+                {cartItems.length == 0 ? <Typography>No items in cart</Typography> :(
                 <>
                 <Box>{itemsToDisplay}</Box>
                 <Button variant='contained' onClick={handleCheckoutClick}>Checkout</Button>
                 </>
             )}
-            {/* <Box>{itemsToDisplay}</Box>
-            <Button variant='contained' onClick={handleCheckoutClick}>Checkout</Button> */}
-       
         </Box>
    
     );
